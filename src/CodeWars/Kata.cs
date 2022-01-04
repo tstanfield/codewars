0c5a3b40ea90da7ec918760987066fbbd785746b
Terry Stanfield <tstanfield@hylaine.com>
2022-01-04 15:11:27 -0500

    works but needs refactor

--- src/CodeWars/Kata.cs
+++ src/CodeWars/Kata.cs
@@ -5,32 +5,113 @@ using System.Text;
 
 namespace CodeWars
 {
-    public class Kata
+    public class Mixing
     {
-        public static int DuplicateCount(string str)
+        public static string Mix(string s1, string s2)
         {
-            var duplicateCharacters = new Dictionary<char, int>();
+            var result = string.Empty;
 
-            str = str.ToLowerInvariant();
+            var duplicateChars = new Dictionary<char, Dictionary<string, int>> { };
 
-            foreach(char c in str)
-            {                
-                if(duplicateCharacters.ContainsKey(c))
+            var q = GetChars(s1, 1)
+                .Concat(GetChars(s2, 2))
+                .GroupBy(
+                    freq => freq,
+                    (key, values) =>
+                        new Tuple<char, int, int>(key.Item1, key.Item2, values.Count())
+                    )
+                .Where(_ => _.Item3 > 1);
+
+            var qq = q.GroupBy(
+                s => s.Item1,
+                (key, values) => new
+                    {
+                        Key = key,
+                        Values = values,
+                        Rank = GetRank(values)
+                    }
+                )
+                .OrderByDescending(_ => _.Values.Max(__ => __.Item3))
+                .ThenBy(_ => _.Rank)
+                .ThenBy(_ => _.Key)
+                ;
+
+            foreach(var x in qq)
+            {
+                var g = x.Values.OrderByDescending(_ => _.Item3);
+
+                var first = g.First();
+                var last = g.Last();
+
+                if (result != string.Empty)
                 {
-                    duplicateCharacters[c] += 1;
+                    result += "/";
+                }
+
+                if(x.Values.Count() == 2 && first.Item3 == last.Item3)
+                {
+                    result += "=:" + new string(first.Item1, first.Item3);                    
                 }
                 else
                 {
-                    duplicateCharacters.Add(c, 0);
+                    result += first.Item2 + ":" + new string(first.Item1, first.Item3);
+                }                
+            }
+
+
+            return result;
+        }
+
+        private static int GetRank(IEnumerable<Tuple<char, int, int>> items)
+        {
+            var g = items.OrderByDescending(_ => _.Item3);
+
+            var first = g.First();
+            var last = g.Last();
+            
+            if(items.Count() == 2 && first.Item3 == last.Item3)
+            {
+                return 99;
+            }
+            else
+            {
+                return first.Item2;
+            }
+        }
+
+        public static List<Tuple<char, int>> GetChars(string str, int order)
+        {
+            var result = str
+                .ToList()
+                .Where(c => char.IsLetter(c) && char.IsLower(c))
+                .Select(c => new Tuple<char, int>(c, order))
+                .ToList();
+
+            return result;
+        }
+
+        public static Dictionary<char, int> GetDuplicateCharCounts(string str)
+        {
+            var result = new Dictionary<char, int> { };
+
+            foreach(var c in str.ToList())
+            {
+                if(!char.IsLower(c) && !char.IsLetter(c))
+                {
+                    continue;
+                }
+
+                if(result.ContainsKey(c))
+                {
+                    result[c] += 1;
+                }
+                else
+                {
+                    result.Add(c, 1);
                 }
             }
 
-            if(duplicateCharacters.Count == 0)
-            {
-                return 0;
-            }
-
-            return duplicateCharacters.Count(_ => _.Value > 0);
+            return result;
         }
     }
 }
--- src/CodeWarsTests/KataTests.cs
+++ src/CodeWarsTests/KataTests.cs
@@ -10,14 +10,20 @@ namespace CodeWars
     public class KataTests
     {
         [Test]
-        public void KataTest()
+        public static void test1()
         {
-            Assert.AreEqual(0, Kata.DuplicateCount(""));
-            Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
-            Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
-            Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
-            Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
-            Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+            Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", 
+                Mixing.Mix("Are they here", "yes, they are here"));
+            Assert.AreEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
+                Mixing.Mix("looping is fun but dangerous", "less dangerous than coding"));
+            Assert.AreEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
+                Mixing.Mix(" In many languages", " there's a pair of functions"));
+            Assert.AreEqual("1:ee/1:ll/1:oo", 
+                Mixing.Mix("Lords of the Fallen", "gamekult"));
+            Assert.AreEqual("", 
+                Mixing.Mix("codewars", "codewars"));
+            Assert.AreEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
+                Mixing.Mix("A generation must confront the looming ", "codewarrs"));
         }
     }
 }
